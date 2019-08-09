a7d60363d889275f70c689b16db11199352409a9
AxelBlaz3 <karthikgaddam4@gmail.com>
2019-08-09 22:25:37 +0530

    beryllium: Added Lawnchair As Prebuilt
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- device.mk
+++ device.mk
@@ -67,3 +67,12 @@ PRODUCT_PACKAGES += \
 
 # GoogleCamera
 $(call inherit-product, packages/apps/GoogleCameraMod/xiaomi-sdm845/config.mk)	
+
+#Lawnchair
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnConf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LOCAL_PATH)/prebuilts/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
